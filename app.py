ada9f9ab6c227aa83cd3a27ff49580c86b667fc3
Lincon Brito <linconjbrito@gmail.com>
2024-08-01 14:12:13 -0300

    Adicionando o sjf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/fifo/submit', methods=['POST'])
 def calc_turnaround_fifo():
     lista_processos = request.json
     tempo_atual = turn_total = 0
@@ -23,6 +23,40 @@ def calc_turnaround_fifo():
          "turnaround" : turn_medio
     }
     return jsonify(response)
+
+
+@app.route('/sjf/submit', methods=['POST'])
+def calc_turnaround_sjf(dicionario):
+        lista_processos = dicionario
+        tempo_atual = turn_total = 0
+        processos_restantes = sorted(lista_processos, key=lambda dicionario: (dicionario['T_chegada'], dicionario['T_exec']))
+        
+        while processos_restantes:
+            processos_disponiveis = []
+            for k, v in enumerate(processos_restantes):
+                if v['T_chegada'] <= tempo_atual:
+                    processos_disponiveis.append(v)
+
+            
+            if not processos_disponiveis:
+                tempo_atual = processos_restantes[0]['T_chegada']
+                processos_disponiveis = []
+                for k, v in enumerate(processos_restantes):
+                    if v['T_chegada'] <= tempo_atual:
+                        processos_disponiveis.append(v)
+
+            processo = min(processos_disponiveis, key=lambda dicionario: dicionario['T_exec'])
+            processos_restantes.remove(processo)
+            
+            tempo_atual += processo['T_exec']
+            processo['Termino'] = tempo_atual  # Atualizado para calcular o tempo de término
+            processo['Turnaround'] = processo['Termino'] - processo['T_chegada']
+            turn_total += processo['Turnaround']
+        
+        turn_medio = turn_total / len(lista_processos)
+        return jsonify(turn_medio) 
+
+
 
 
 if __name__ == '__main__':
